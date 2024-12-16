8b9da836b6ca97921169c81c2f22155c0a9f0a74
Ola Lundén <ola@kodamera.se>
2024-12-16 13:10:16 +0100

    Fix live edit of initiative

--- components/PlayersList.vue
+++ components/PlayersList.vue
@@ -24,7 +24,7 @@
         </div>
         <div class="shrink-0 w-14 h-full flex items-center justify-center text-center text-2xl rounded bg-white/10 border-l border-t border-gray-400 z-40 font-semibold">
           <span v-if="editingIndex !== player.id" @click="editInitiative(player.id)">{{ player.initiative }}</span>
-          <input v-else type="tel" v-model.number="newInitiative" @blur="saveInitiative(index)" @keyup.enter="saveInitiative(index)" class="w-full h-full rounded text-center bg-white/90 border-none focus:outline-none">
+          <input v-else type="tel" v-model.number="newInitiative" @blur="saveInitiative(player.id)" @keyup.enter="saveInitiative(player.id)" class="w-full h-full rounded text-center bg-white/90 border-none focus:outline-none">
         </div>
       </li>
     </ul>
@@ -62,17 +62,24 @@ function closeConditionsModal() {
   conditionsModal.value = false
 }
 
-function editInitiative(index) {
-  editingIndex.value = index
-  newInitiative.value = store.players[index].initiative
+/**
+ * @param pid Player ID
+ */
+function editInitiative(pid) {
+  let player = store.getPlayerById(pid)
+  editingIndex.value = pid
+  newInitiative.value = player.initiative
 }
 
-function saveInitiative(index) {
+/**
+ * @param pid Player ID
+ */
+function saveInitiative(pid) {
   // Only save if the value is a number
   if (parseInt(newInitiative.value)) {
-    store.players[index].initiative = newInitiative.value
-    store.sortPlayers()
-    store.saveToLocalStorage()
+    let player = store.getPlayerById(pid)
+    player.initiative = newInitiative.value
+    store.updatePlayer(player)
   }
 
   editingIndex.value = null
